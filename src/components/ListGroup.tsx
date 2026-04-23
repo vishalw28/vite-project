b21644d32e2b621d37bc34810c9cf5fcae9b58b0
Vishal Wagh <vishalw28@hotmail.com>
2026-04-23 11:53:38 +0530

    render list group dynamically.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,15 @@
 
 function ListGroup() {
+
+  const items = [
+    'Bangalore',
+    'Pune',
+    'Mumbai',
+    'Chennai',
+    'Delhi'
+  ];  
+
+
   return (
     /**
      * In react component can't return more than one element.
@@ -8,16 +18,7 @@ function ListGroup() {
     // 
     // Wrap using abbrevation, which creates the another element in dom
     // <div> 
-    //     <h1>List</h1>
-    //     <ul className="list-group">
-    //       <li className="list-group-item active" aria-current="true">
-    //         An active item
-    //       </li>
-    //       <li className="list-group-item">A second item</li>
-    //       <li className="list-group-item">A third item</li>
-    //       <li className="list-group-item">A fourth item</li>
-    //       <li className="list-group-item">And a fifth one</li>
-    //     </ul>
+    //    ....
     // </div> 
 
     // Using this we'll not have additional element on the screen.
@@ -25,13 +26,7 @@ function ListGroup() {
         <>
             <h1>List</h1>
             <ul className="list-group">
-              <li className="list-group-item active" aria-current="true">
-                An active item
-              </li>
-              <li className="list-group-item">A second item</li>
-              <li className="list-group-item">A third item</li>
-              <li className="list-group-item">A fourth item</li>
-              <li className="list-group-item">And a fifth one</li>
+                {items.map(item => <li key={item}>{item}</li>)}
             </ul>
         </>
     
