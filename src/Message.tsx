277ef1f8b24a855d4588a1bacde873f94f46ff38
Vishal Wagh <vishalw28@hotmail.com>
2026-04-23 11:21:00 +0530

    React initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,15 @@
+// Pascalcasing
+function Message(){
+    // JSX: Java Script 
+    /**
+     * https://babeljs.io to see how code is converted to javascript xml
+     * 
+     */
+    const name = '';
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+    
+}
+
+export default Message;
